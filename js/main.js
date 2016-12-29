b5f59d87333f517ffc7ebcd0beb27065fdecab53
apalm112 <apalm112@gmail.com>
2016-12-29 15:18:23 -0800

    Updated function syntax to ES2015 standards.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function setInitialFocus() {
   $setPageLoadFocus.focus();
 }
 
-function createOtherJobTextarea() {
+const createOtherJobTextarea = () => {
   // DONE: A text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.  Give the field an id of “other-title,” and add the placeholder text of "Your Job Role" to the field.
   // TODO: Fix error thrown to console when else clause conditional runs while there is No input text to remove.
   // TODO: Fix margins on newTextArea to fit better onto the form.
