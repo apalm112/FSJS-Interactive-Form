c12532d710afc12fd32d25c523adc6e92d7dec1e
apalm112 <apalm112@gmail.com>
2016-12-29 07:25:04 -0800

    Changed set focus into a function, started work on code for other job role function.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,48 @@
+/* Treehouse Project-03: Build an Interactive Form */
+
 // DONE: Set focus first text field on page load, vanilla JS works.
-var giveInputNameFocus = document.getElementById('name');
-function setFocus() {
-  giveInputNameFocus.focus();
-}
-setFocus();
+// var giveInputNameFocus = document.getElementById('name');
+// function setFocus() {
+//   giveInputNameFocus.focus();
+// }
+// setFocus();
 
 // DONE: Set focus on first text field on page load w/ jQuery.
-var $setPageLoadFocus = $('#name');
-$setPageLoadFocus.focus();
-
-// TODO: Job Role Section-  when 'other' option is selected from 'Job Role'
-        // reveal a text field w/ id="other-title" placeholder="Your Job Role"
-var $formFirstChild = $('form:first-child');
-var $newTextArea = $('input');
-$newTextArea.attr(type="text"  id="other-title" placeholder="Your Job Role");
-if (select.#title.value === 'other') {
-  $formFirstChild.append($newTextArea);  
+function setInitialFocus() {
+  var $setPageLoadFocus = $('#name');
+  $setPageLoadFocus.focus();
 }
+
+function createOtherJobTextarea() {
+  // TODO: Job Role Section-  when 'other' option is selected from 'Job Role'
+  // reveal a text field w/ id="other-title" placeholder="Your Job Role"
+  var $formFirstChild = $('form:first-child');
+  var $newTextArea = $('input');
+
+// something like
+  $('').on('click', function() {
+    $newTextArea.attr(type='text', id='other-title', name='user_other_role', placeholder='Your Job Role');
+    if (select.title.value === 'other') {
+      $formFirstChild.append($newTextArea);
+    }
+  });
+  // #title.on('click', )
+
+  // NOT working:
+  // var $getForm = $('form');
+  // var getField = $getForm.children();
+  // var setOtherInput = getField[0];
+  // var otherInput = '<input type="text" id="other-title" placeholder="Your Job Role">';
+  // setOtherInput.append(otherInput)
+  //
+  //
+  // DOES NOT Work: but WAS?????
+  // var $getForm = $('form');
+  // var getField = $getForm.children();
+  // var otherInput = '<input type="text" id="other-title" placeholder="Your Job Role">';
+  // getField[0].append(otherInput)
+}
+
+$(document).ready(function() {
+  setInitialFocus();
+});
