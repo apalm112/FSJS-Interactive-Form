d93bdcc531db6d8508a86f1138562c07966157c9
apalm112 <apalm112@gmail.com>
2016-12-29 14:48:00 -0800

    Updated the TODO's for this function.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 /* Treehouse Project-03: Build an Interactive Form */
 
-// DONE: Set focus first text field on page load, vanilla JS works.
-// var giveInputNameFocus = document.getElementById('name');
-// function setFocus() {
-//   giveInputNameFocus.focus();
-// }
-// setFocus();
 
 // DONE: Set focus on first text field on page load w/ jQuery.
 function setInitialFocus() {
@@ -14,8 +8,8 @@ function setInitialFocus() {
 }
 
 function createOtherJobTextarea() {
-  // TODO: Job Role Section-  when 'other' option is selected from 'Job Role'
-  // reveal a text field w/ id="other-title" placeholder="Your Job Role"
+  // TODO: Fix error thrown to console when else clause conditional runs while there is No input text to remove.
+  // TODO: Fix margins on newTextArea to fit better onto the form.
   let getSelectJobRole = document.getElementById('title');
   let newTextArea = document.createElement('input');
   newTextArea.setAttribute('id', 'other-title');
