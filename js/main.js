04e2131d55acb290d624d4c2ee0b6b7ed0810282
apalm112 <apalm112@gmail.com>
2016-12-28 20:06:37 -0800

    Added .eslintrc file to project, started psuedo code for the Job Role Section goals.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-// TODO: Set focus first text field on page load.
+// DONE: Set focus first text field on page load, vanilla JS works.
 var giveInputNameFocus = document.getElementById('name');
 function setFocus() {
   giveInputNameFocus.focus();
 }
 setFocus();
 
-// TODO: Set focus on first text field on page load w/out jQuery.
+// DONE: Set focus on first text field on page load w/ jQuery.
 var $setPageLoadFocus = $('#name');
 $setPageLoadFocus.focus();
+
+// TODO: Job Role Section-  when 'other' option is selected from 'Job Role'
+        // reveal a text field w/ id="other-title" placeholder="Your Job Role"
+var $formFirstChild = $('form:first-child');
+var $newTextArea = $('input');
+$newTextArea.attr(type="text"  id="other-title" placeholder="Your Job Role");
+if (select.#title.value === 'other') {
+  $formFirstChild.append($newTextArea);  
+}
